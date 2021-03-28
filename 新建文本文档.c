1f5ea0bedffec7c058d06d7434d16670e705bf22
swf-not-y <81173491+swf-not-y@users.noreply.github.com>
2021-03-28 19:56:50 +0800

    Update 新建文本文档.c

--- 新建文本文档.c
+++ 新建文本文档.c
@@ -1,9 +1,9 @@
-ums = [1,2,3,4,5,6,7,8,9]
+nums = [1,2,3,4,5,6,7,8,9]
 target = 10
 for i in nums:
     for j in nums:
         if(nums[i] + nums[j] == target):
-            print("�б�Ϊ%d,%d��",i,j)
+            print("ÁÐ±íÎª%d,%d£º",i,j)
             nums1 = [i,j]
             print(nums1)
 print(nums1)
