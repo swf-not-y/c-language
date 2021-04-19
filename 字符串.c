5a956586c7593d67df11b74c798971a638be83da
swf-not-y <1873366265@qq.com>
2021-04-19 22:30:07 +0800

    Create 字符串.c

---
+++ 字符串.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+	char arr1[] = "À´×ÔĞÇĞÇµÄÄã";
+	char arr2[] = "      ";
+	int left = 0;
+	int mid = 0;
+	int right = strlen(arr1) - 1;
+	mid = (left + right) / 2;
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);
+		system("cls");
+		left++;
+		right--;
+
+	}
+	printf("%s\n", arr2);
+	return 0;
+}
